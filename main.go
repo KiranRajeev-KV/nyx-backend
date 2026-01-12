d13a15b4590fa367a1d02c27a8a44bd8a5cd0147
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-12 14:50:25 +0530

    feat: implement basic server with Gin framework

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!!")
+	router := gin.Default()
+	fmt.Println("Starting server on port 8080...")
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Server is running!",
+		})
+	})
+	router.Run()
 }
