8308b4dfee9203542f70f1c15b0585ab69f3b64e
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-12 16:12:19 +0530

    feat: add server initialization and graceful shutdown

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func StartServer() {
 	router := gin.Default()
 	fmt.Println("Starting server on port 8080...")
 	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "Server is running!",
 		})
 	})
-	router.Run()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		fmt.Println("[OK]: Start the server on port " + ":8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("could not listen on port "+":8080", err)
+		} // Blocking in nature
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("[OK]: Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Println("[OK]: Server forced to shutdown", err)
+	}
+}
+
+func main() {
+	StartServer()
 }
