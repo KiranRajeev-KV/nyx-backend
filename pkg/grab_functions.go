e22ab0a3b083b85f52d4b3ed7ae26d5864fde071
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-13 18:11:55 +0530

    feat: add GetEmail function to retrieve email from context

--- pkg/grab_functions.go
+++ pkg/grab_functions.go
@@ -1,6 +1,10 @@
 package pkg
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/KiranRajeev-KV/nyx-backend/internal/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/ksuid"
 )
@@ -9,4 +13,19 @@ func TagRequestWithId(c *gin.Context) {
 	id := ksuid.New()
 	c.Set("request_id", id.String())
 	c.Next()
+}
+
+func GetEmail(c *gin.Context, route string) (string, bool) {
+	email := c.GetString("email")
+	if email == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Oops! Something happened. Please try again later.",
+		})
+		msg := fmt.Sprintf("[%s-FATAL]: No email after crossing auth middleware", route)
+		logger.Log.FatalCtx(c, msg, nil)
+
+		return "", false
+	}
+
+	return email, true
 }
