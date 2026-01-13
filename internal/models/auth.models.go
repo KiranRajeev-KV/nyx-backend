6ed66811a32c646ad285af14709cfec7908d4b24
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-13 17:32:04 +0530

    feat: add RegisterUserRequest model with validation

---
+++ internal/models/auth.models.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	v "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type RegisterUserRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (r RegisterUserRequest) Validate() error {
+	return v.ValidateStruct(&r,
+		v.Field(&r.Name, v.Required, v.Length(3, 100)),
+		v.Field(&r.Email, v.Required, is.Email),
+		v.Field(&r.Password, v.Required, v.Length(8, 128)),
+	)
+}
