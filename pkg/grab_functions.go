9aa3ddb2fddc17f58d717298ca4cb3b29ecd3d57
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-13 15:16:57 +0530

    ref: move GrabRequestId from pkg to logger

--- pkg/grab_functions.go
+++ pkg/grab_functions.go
@@ -1,8 +1,6 @@
 package pkg
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/ksuid"
 )
@@ -11,12 +9,4 @@ func TagRequestWithId(c *gin.Context) {
 	id := ksuid.New()
 	c.Set("request_id", id.String())
 	c.Next()
-}
-
-func GrabRequestId(c *gin.Context) string {
-	reqId, ok := c.Get("request_id")
-	if !ok {
-		return "missing-id"
-	}
-	return fmt.Sprintf("%v", reqId)
 }
